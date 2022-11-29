9a99e17557edd4276777ebff9b6aa990ca26671a
shubhoahmed <shubhoahmed03@gmail.com>
2022-11-29 06:47:13 +0600

    question & ans added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -5,7 +5,15 @@ const Blog = () => {
         <div>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    What are the different ways to manage a state in a React application?
+                </div>
+                <div className="collapse-content">
+                    <p> React state management is a process for managing the data that React components need in order to render themselves. This data is typically stored in the component's state object. When the state object changes, the component will re-render itself. React state management is basically half of a React app.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    How does prototypical inheritance work?
                 </div>
                 <div className="collapse-content">
                     <p>tabIndex={0} attribute is necessary to make the div focusable</p>
@@ -13,7 +21,7 @@ const Blog = () => {
             </div>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    What is a unit test? Why should we write unit tests?
                 </div>
                 <div className="collapse-content">
                     <p>tabIndex={0} attribute is necessary to make the div focusable</p>
@@ -21,18 +29,10 @@ const Blog = () => {
             </div>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    React vs. Angular vs. Vue?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-                </div>
-            </div>
-            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-                <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
-                </div>
-                <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>Vue provides higher customizability and hence is easier to learn than Angular or React. Further, Vue has an overlap with Angular and React with respect to their functionality like the use of components. Hence, the transition to Vue from either of the two is an easy option.</p>
                 </div>
             </div>
         </div>
